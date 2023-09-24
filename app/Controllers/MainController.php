b3b2069707b4ad6ba003405a6ea37786d875d34c
christia25 <delachicachristiaangelicam@gmail.com>
2023-09-24 15:12:56 +0800

    main

---
+++ app/Controllers/MainController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class MainController extends BaseController
+{
+    private $music;
+    private $play;
+    public function __construct(){
+        $this->music = new \App\Models\SongModel();
+        $this->play = new \App\Models\PlaylistModel();
+    }
+    public function index()
+    {
+        //
+    }
+    public function song()
+    {
+        $data=[
+            'music' => $this->music->findAll(),
+            'play' => $this->play->findAll()
+        ];
+        return view ('song', $data);
+    }
+    public function save(){
+        $data = ['playlist' => $this->request->getVar('playlist')];
+        return redirect()->to('chris');
+    }
+    
+}
